b40bf62980a483d281497e788bcc29577e4b70d3
Jonas Souza <jhonask25@gmail.com>
2023-10-19 23:40:26 -0300

    tamplates

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 
+  namePortal: string;
+
+  courses: string[] = ['Java', 'Ext Js', 'Angular'];
+
+  constructor() {
+    this.namePortal = "http://loiane.training";
+  }
+  ngOnInit() {
+  }
 }
